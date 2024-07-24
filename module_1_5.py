a60097cc42f605cc73da1c94602125f0f981ac8d
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-24 11:32:01 +0300

    Неизменяемые и изменяемые объекты. Кортежи и списки
    Неизменяемые и изменяемые объекты. Кортежи и списки

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,4 @@
 # Неизменяемые и изменяемые объекты. Кортежи и списки
-
 # Задайте переменные разных типов данных:
 # Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
 # Выполните операции вывода кортежа immutable_var на экран.
@@ -9,18 +8,19 @@
 # Создайте переменную mutable_list и присвойте ей список из нескольких элементов.
 # Измените элементы списка mutable_list.
 # Выведите на экран измененный список mutable_list.
-
 # Пример результата выполнения программы:
 # Immutable tuple: (1, 2, 'a', 'b')
 # Mutable list: [1, 2, 'a', 'b', 'Modified']
 
 immutable_var = (2023, 2024, 'abc', False)
 print('Immutable tuple: ', immutable_var)
+
 # immutable_var[0][0]=2020 # неизменяемый тип
 # изменить можно лишь элемент списка, который находится внутри кортежа
 # (immutable_var)=([20, 21, 22], 23)
 # immutable_var[0][0]=19
 # print(immutable_var)
+
 mutable_list = [1, 2, 3, 'letter', True]
 print('Mutable list: ', mutable_list)
 mutable_list.append(20)
